5d8fd5b51cb3e8218e75828fbe45a4b151bdac6c
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-04-03 21:54:03 +0600

    add: add dom elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillioners = document.getElementById("show-millioners");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
