b148a547eed7c90bbbd78938225599ef40d8c3a3
Muhammed Abdrahman <muhammed03amin@gmail.com>
2021-04-10 08:00:09 +0600

    add: add async function to get random user data

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const doubleBtn = document.getElementById("double");
 const showMillioners = document.getElementById("show-millioners");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch random user and add money
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+}
